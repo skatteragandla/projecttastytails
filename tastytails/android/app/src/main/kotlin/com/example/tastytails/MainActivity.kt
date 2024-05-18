3059f4e17244c6cc804807db9b4e02670200c1f5
saiaravindreddy <saiaravindreddy10@gmail.com>
2024-05-17 20:12:23 -0700

    Adding APK File

--- tastytails/android/app/src/main/kotlin/com/example/tastytails/MainActivity.kt
+++ tastytails/android/app/src/main/kotlin/com/example/tastytails/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.example.tastytails
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.webviewflutter.WebViewFlutterPlugin
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        WebViewFlutterPlugin.registerWith(flutterEngine.dartExecutor.binaryMessenger)
-    }
 }
